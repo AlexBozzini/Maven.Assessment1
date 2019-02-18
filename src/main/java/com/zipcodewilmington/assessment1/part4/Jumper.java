05207629af043fffe0590af70b1bc7a11164cb22
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-02-18 12:41:42 -0500

    jumper done

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,6 +6,8 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int k, int j) {
-        return -1;
+        Integer typeOneJumps = k / j;
+        Integer typeTwoJumps = k % j;
+        return typeOneJumps + typeTwoJumps;
     }
 }
