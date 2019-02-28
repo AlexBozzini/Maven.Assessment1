075e3feb34aa53576ff24ee0d1b602debbb6b279
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-02-28 16:27:12 -0500

    Passed all remaining tests

--- src/main/java/com/zipcodewilmington/assessment1/part3/Dog.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Dog.java
@@ -4,25 +4,28 @@ package com.zipcodewilmington.assessment1.part3;
  * Created by leon on 2/16/18.
  */
 public class Dog extends Pet {
+    private String name;
+    private int age;
     /**
      * @param name name of this Dog
      * @param age age of this dog
      */
     public Dog(String name, Integer age) {
-
+        super(name, age);
     }
 
     /**
      * @param age age of this dog
      */
     public Dog(Integer age) {
+        super("Dog name", age);
     }
 
     /**
      * @param name name of this dog
      */
     public Dog(String name) {
-
+        super(name);
     }
 
     /**
@@ -32,6 +35,7 @@ public class Dog extends Pet {
      * age is 0
      */
     public Dog() {
+        super("Dog name");
     }
 
     /**
